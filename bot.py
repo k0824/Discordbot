66fa877831cd78db2c12208a05483b1c3fccd736
Ko <k0824.l.y.r@gmail.com>
2018-10-09 11:42:10 +0900

    idolinfo 以外はoldと同等の機能を実装

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord  # discord 用パッケージ
 import asyncio  # asyncIO
-from cls import Tachibana  # tachibana
-
+import cls  # 各クラス
+import threading
 
 client = discord.Client()  # 接続用オブジェクト
 token = 'NDk1NTU2NzExNzQ3Mjg5MDk4.DpDy1A.a1oUwLkPtvXuq72B9juG8a8TO1s'  # botアカウントのアクセストークン
@@ -18,16 +18,47 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
+	# /remind
+	if message.content.startswith('/remind'):
+		remind_thread = threading.Thread(target=cls.Reminder.remind, args=(message, client, loop))
+		remind_thread.start()
+
+	# /event ### YYMMDD で YYMMDD日開始の###イベントに対してリマインダーを作成
+	if message.content.startswith('/event'):
+		event_thread = threading.Thread(target=cls.Event.remind, args=(message, client, loop))
+		event_thread.start()
+		await client.send_message(message.channel, 'イベントスケジュールを読み込みます......')
+
 	# /neko
 	if message.content.startswith('/neko'):
-		await client.send_message('にゃーん')
+		await client.send_message(message.channel, 'にゃーん')
 
 	# 'ありす' に反応
-	if 'ありす' in message.content and client.user != message.outhor and Tachibana.val is True:
-		await client.send_message(Tachibana.reply())
+	if 'ありす' in message.content and client.user != message.author and cls.Tachibana.val is True:
+		await client.send_message(message.channel, cls.Tachibana.reply())
 	# /tachibana によるトグル
 	if message.content.startswith('/tachibana'):
-		Tachibana.toggle()
+		cls.Tachibana.toggle()
+		if cls.Tachibana.val is True:
+			await client.send_message(message.channel, '橘を有効化しました')
+		else:
+			await client.send_message(message.channel, '橘を無効化しました')
+
+	# /clear でログを全削除
+	if message.content.startswith('/clear'):
+		await client.send_message(message.channel, 'チャンネル内のログをすべて消去します。よろしければ yes を送信してください')
+		msg = await client.wait_for_message(timeout=30, author=message.author, channel=message.channel)
+		if msg.content == 'yes':
+			clean_flag = True
+			while clean_flag is True:
+				msgs = [msg async for msg in client.logs_from(message.channel)]
+				if len(msgs) > 1:  # 1発言以下でdelete_messagesするとエラーになるため
+					await client.delete_messages(msgs)
+				else:
+					clean_flag = False
+					await client.send_message(message.channel, 'ログの全削除が完了しました')
+		else:
+			await client.send_message(message.channel, '処理を中断しました')
 
 # botの起動と接続
 client.run(token)
--- cls.py
+++ cls.py
@@ -1,4 +1,17 @@
 import random
+import asyncio
+import time
+from datetime import datetime
+import sched
+import event
+
+
+class Send:  # メッセージ送信用クラス
+	@staticmethod
+	def send(msg, client, loop, reply=None):
+		if reply is None:
+			reply = msg.content
+		asyncio.ensure_future(client.send_message(msg.channel, reply), loop=loop)
 
 
 class Tachibana:  # 橘です！用クラス
@@ -10,8 +23,55 @@ class Tachibana:  # 橘です！用クラス
 
 	@classmethod
 	def toggle(cls):
-		cls.val is not cls.val
+		cls.val = not cls.val
 
 	@classmethod
 	def reply(cls):
 		return random.choice(cls.reply_list)
+
+
+class Reminder:  # リマインダー用クラス
+	@staticmethod
+	def remind(msg, client, loop):
+		run_at = msg.content[8:18]
+		run_at = '20' + run_at
+		try:
+			run_at = datetime.strptime(run_at, '%Y%m%d%H%M')
+		except:
+			reply = '入力形式が間違っています。2018/7/31 20:30 にリマインダーを設定したい場合は、 /remind 1807312030 リマインド時に送信する文言 のように入力してください。'
+			asyncio.async(client.send_message(msg.channel, reply), loop=loop)
+			return 0
+		if run_at < datetime.now():
+			asyncio.async(client.send_message(msg.channel, '入力した日時が過去のものです。'), loop=loop)
+			return 0
+		run_at = int(time.mktime(run_at.utctimetuple()))
+		msg.content = msg.content[19:]
+		if msg.content == '':
+			msg.content = msg.author.mention + ' リマインダーです'
+		else:
+			msg.content = msg.author.mention + ' ' + msg.content
+		scheduler = sched.scheduler(time.time, time.sleep)
+		scheduler.enterabs(run_at, 1, Send.send, argument=(msg, client, loop))
+		asyncio.async(client.send_message(msg.channel, 'リマインダーを設定しました。'), loop=loop)
+		scheduler.run()
+
+
+class Event:  # イベントスケジューラ用クラス
+	@staticmethod
+	def remind(msg, client, loop):
+		global event_schedule
+		time_now = datetime.now()
+		event_scheduler = sched.scheduler(time.time, time.sleep)
+		event_type = msg.content[7:10]
+		if event_type == 'dlf':
+			event_schedule = event.dlf(msg.content)
+		elif event_type == 'tbs':
+			event_schedule = event.tbs(msg.content)
+		if event_schedule is False:
+			asyncio.async(client.send_message(msg.channel, '入力形式が間違っています。 /event dlf 180731 のように入力してください。'), loop=loop)
+			return 0
+		for remind_time, remind_msg in event_schedule.items():
+			if remind_time > int(time.mktime(time_now.utctimetuple())):
+				event_scheduler.enterabs(remind_time, 1, Send.send, argument=(msg, client, loop, remind_msg))
+		asyncio.async(client.send_message(msg.channel, 'イベントスケジュールを正常に読み込みました。'), loop=loop)
+		event_scheduler.run()
